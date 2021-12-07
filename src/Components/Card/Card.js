a244ddef6743e119548ca3dd2c0446506fdc11a4
bin.zhang02 <bin.zhang02@weimob.com>
2021-12-07 22:22:31 +0800

    type: feature
    卡片支持反转

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import './index.css'
+const defaultImg = 'http://faith-2021-001.oss-cn-hangzhou.aliyuncs.com/20211207/1638868600827311.svg'
 const Card= (props) => {
   console.log('faith=============', props);
-
   return (
-    // <div  className="flip-container">
-    //   {/*<div className="flipper">*/}
-    //   {/*  <div className="front" style={{ background: 'red' }} >*/}
-    //   {/*    <p>正面12</p>*/}
-    //   {/*  </div>*/}
-    //   {/*  <div className="back" style={{ background: 'yellow' }} >*/}
-    //   {/*    <p>背面</p>*/}
-    //   {/*  </div>*/}
-    //   {/*</div>*/}
-    // </div>
-    <div  className="zhangbin" style={{ margin: '5px', border: '1px solid #dadce0', display: 'inline-block', background: 'white', borderRadius: '5px', minWidth: 100, minHeight: 50 }} >{props.children}</div>
+    <div className="wrap">
+      <div className="card">
+        <div className="front">{props?.front}
+          <div className='front-wrap'>
+            <img className='front-img' src= {props.src ? props.src : defaultImg} alt='' />
+          </div>
+
+        </div>
+        <div className="back">{props?.back}</div>
+      </div>
+    </div>
   )
 }
 
