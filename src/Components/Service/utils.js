9f0fc6c23797e62c89eaae6f3a8b3297e79b516a
bin.zhang02 <bin.zhang02@weimob.com>
2021-12-07 22:21:51 +0800

    rollup 库支持js库封装的公共方法 FUtils
    FUtils： Faith的Utils

---
+++ src/Components/Service/utils.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    return a + b;
+}
+
+// 方法
+const FUtils = {
+    sum
+}
+export default FUtils
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import Switch from './Components/Switch'
 import Card from './Components/Card/Card'
-export {Switch, Card}
-
-
+import FUtils from './Components/Service/utils'
+export {Switch, Card, FUtils}
 
 // 以下写法可以简化成
 // import Switch from './Components/Switch'
